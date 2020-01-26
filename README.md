bbfb3497cd21d75edf82ad5ea80349ad70a5bed4
mizokami <r.mizokami@gmail.com>
2020-01-26 18:26:18 +0900

    Init plugin

---
+++ README.md
@@ -0,0 +1,31 @@
+# bitbar-remaining-time-to-spend-with-my-children
+
+BitBar plugin to show remaining time to spend with your children
+
+<img width="371" src="https://user-images.githubusercontent.com/1257116/73133798-14a41080-4071-11ea-941f-f0f3d3a9d70f.png">
+
+## Configuration
+
+`~/.bitbarrc` example:
+
+```ini
+[remaining_time_to_spend_with_my_children]
+
+;# Required
+child_identifiers = child0,child1
+
+child0_label            = ":girl:"
+child0_birthday         = "2014-07-10+09:00"
+child0_independence_day = "2032-04-01+09:00"
+
+child1_label            = ":baby:"
+child1_birthday         = "2019-12-01+09:00"
+child1_independence_day = "2037-04-01+09:00"
+
+;# Optional
+hours_a_day_during_infant             = 7
+hours_a_day_during_elementary         = 6
+hours_a_day_during_junior_high_school = 4
+hours_a_day_during_high_school        = 3
+hours_a_day_during_college_or_later   = 2
+```
---
+++ remaining-time-to-spend-with-my-children.1h.rb
@@ -0,0 +1,242 @@
+#!/usr/bin/env ruby
+require 'erb'
+require 'time'
+
+module BitBar
+  class INIFile
+    Error = Class.new(StandardError)
+
+    INIFileNotFound = Class.new(Error)
+
+    SectionNotFound = Class.new(Error)
+
+    def self.load(file = "#{ENV['HOME']}/.bitbarrc", base: {})
+      raise INIFileNotFound if !File.exist?(file)
+
+      parse(open(file) { |f| f.read })
+    end
+
+    def self.parse(source)
+      # XXX: This implementation isn't correct, but will work in most cases.
+      #      (Probably `StringScanner` will make code correct and clean.)
+      sections = {}
+
+      section = nil
+
+      source.each_line do |line|
+        if line =~ /^ *;/
+          # comment
+          next
+        end
+
+        if line =~ /^\[(.+)\]$/
+          section = sections[$1.to_sym] = {}
+          next
+        end
+
+        next unless section
+
+        if line =~ /(.+?)=(.+)$/
+          name  = $1.strip.to_sym
+          value = $2.strip
+
+          section[name] = value[/^"(.*)"$/, 1] || value[/^'(.*)'$/, 1] || value
+          next
+        end
+      end
+
+      new(sections: sections)
+    end
+
+    def initialize(sections:)
+      @sections = sections
+    end
+
+    def fetch(name, *options)
+      @sections.fetch(name.to_sym, *options)
+    rescue KeyError
+      raise SectionNotFound
+    end
+  end
+
+  module RemainingTimeToSpendWithMyChildren
+    ConfigurationError = Class.new(StandardError)
+
+    class Children < Struct.new(:label,
+                                :birthday,
+                                :independence_day,
+                                :hours_a_day_during_infant,
+                                :hours_a_day_during_elementary,
+                                :hours_a_day_during_junior_high_school,
+                                :hours_a_day_during_high_school,
+                                :hours_a_day_during_college_or_later)
+      def self.from(children, options:)
+        children.map do |child|
+          base_keys = %i[
+            label
+            birthday
+            independence_day
+          ]
+
+          option_keys = %i[
+            hours_a_day_during_infant
+            hours_a_day_during_elementary
+            hours_a_day_during_junior_high_school
+            hours_a_day_during_high_school
+            hours_a_day_during_college_or_later
+          ]
+
+          Children.new(
+            *base_keys.map {|key| child.fetch(key) },
+            *option_keys.map {|key| options.fetch(key) },
+          )
+        end
+      end
+
+      # FIXME: Should consider "早生まれ".
+      def remaining_hours
+        hours = 0
+
+        today = Date.today
+
+        i = Date.new(birthday.year + 6, 4, 1)
+        t = (i - today).to_i
+        hours += t * hours_a_day_during_infant if t > 0
+
+        e = Date.new(birthday.year + 12, 4, 1)
+        t = (e - [i, today].max).to_i
+        hours += t * hours_a_day_during_elementary if t > 0
+
+        j = Date.new(birthday.year + 15, 4, 1)
+        t = (j - [e, today].max).to_i
+        hours += t * hours_a_day_during_junior_high_school if t > 0
+
+        h = Date.new(birthday.year + 18, 4, 1)
+        t = (h - [j, today].max).to_i
+        hours += t * hours_a_day_during_high_school if t > 0
+
+        t = (independence_day - [j, today].max).to_i
+        hours += t * hours_a_day_during_college_or_later if t > 0
+
+        hours
+      end
+
+      def remaining_days
+        (independence_day - Date.today).to_i
+      end
+    end
+
+    class View
+      TEMPLATE = <<-EOT.gsub(/^ */, '')
+        <%= @children[0].label %><%= @children[0].remaining_days %> days | font=courier color=black
+        ---
+        <% @children.each do |children| -%>
+        <%= children.label %> <%= children.remaining_days %> days (<%= children.remaining_hours %> hours) | font=courier color=black
+        <% end -%>
+      EOT
+
+      def initialize(children:)
+        @children = children
+      end
+
+      def render
+        puts ERB.new(TEMPLATE, nil, '-').result(binding)
+      end
+    end
+
+    class App
+      DEFAULT_CONFIG = {}.freeze
+
+      def initialize(config = {})
+        @config = cast_config(DEFAULT_CONFIG.merge(config))
+      end
+
+      def run
+        children = Children.from(@config.fetch(:children), options: @config)
+
+        View.new(children: children).render
+      end
+
+      private
+
+      def cast_config(config)
+        config[:child_identifiers] = config.fetch(:child_identifiers).split(',')
+
+        config[:children] = config[:child_identifiers].map do |identifier|
+          label            = :"#{identifier}_label"
+          birthday         = :"#{identifier}_birthday"
+          independence_day = :"#{identifier}_independence_day"
+
+          {
+            label:            config.fetch(label),
+            birthday:         Date.parse(config.fetch(birthday)),
+            independence_day: Date.parse(config.fetch(independence_day))
+          }
+        end
+
+        %i[
+          hours_a_day_during_infant
+          hours_a_day_during_elementary
+          hours_a_day_during_junior_high_school
+          hours_a_day_during_high_school
+          hours_a_day_during_college_or_later
+        ].each { |key| config[key] = config.fetch(key, 24).to_i }
+
+        config
+      rescue KeyError => e
+        raise ConfigurationError, "Required key missing - #{e.message}"
+      rescue ArgumentError
+        raise ConfigurationError, 'Date format might be invalid.'
+      end
+    end
+  end
+end
+
+if __FILE__ == $0
+  begin
+    config = BitBar::INIFile
+      .load
+      .fetch(:'remaining_time_to_spend_with_my_children', {})
+
+    if File.exists?("#{ENV['HOME']}/.bitbarrc.local")
+      local_config = BitBar::INIFile
+        .load("#{ENV['HOME']}/.bitbarrc.local")
+        .fetch(:'remaining_time_to_spend_with_my_children', {})
+
+      config.merge!(local_config)
+    end
+
+    raise BitBar::INIFile::Error if config.empty?
+
+    BitBar::RemainingTimeToSpendWithMyChildren::App.new(config).run
+  rescue BitBar::INIFile::Error
+    puts <<-EOM.gsub(/^ */, '')
+      ⚠️
+      ---
+      To setup, create or edit your ~/.bitbarrc file with a new section, like: | color=black
+      |
+      [remaining_time_to_spend_with_my_children] | font=courier color=black
+      ;# Required                                           | font=courier color=black
+      child_identifiers       = child0,child1               | font=courier color=black
+      child0_label            = ":girl:"                    | font=courier color=black
+      child0_birthday         = "2017-04-05+09:00"          | font=courier color=black
+      child0_independence_day = "2035-04-01+09:00"          | font=courier color=black
+      child1_label            = ":boy:"                     | font=courier color=black
+      child1_birthday         = "2019-04-05+09:00"          | font=courier color=black
+      child1_independence_day = "2037-04-01+09:00"          | font=courier color=black
+      |
+      ;# Optional                                           | font=courier color=black
+      hours_a_day_during_infant             = 5             | font=courier color=black
+      hours_a_day_during_elementary         = 5             | font=courier color=black
+      hours_a_day_during_junior_high_school = 3             | font=courier color=black
+      hours_a_day_during_high_school        = 2             | font=courier color=black
+      hours_a_day_during_college_or_later   = 2             | font=courier color=black
+    EOM
+  rescue BitBar::RemainingTimeToSpendWithMyChildren::ConfigurationError => e
+    puts <<-EOM.gsub(/^ */, '')
+      ⚠️
+      ---
+    #{e.message}
+    EOM
+  end
+end
